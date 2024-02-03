3bd6ae89389cfc6a1668972414bbbb6ad0d88474
Palak Gupta <120472704+Palakgupta2002@users.noreply.github.com>
2024-02-03 19:23:42 +0530

    Process

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const MODEL_NAME = "gemini-pro";
-const API_KEY = "AIzaSyDvtOMWfBiFP8gSzbaYw5fDYRX9Av-_B4I";
+const API_KEY = "API KEY";
 
 async function runChat(userInput) {
   const genAI = new GoogleGenerativeAI(API_KEY);
